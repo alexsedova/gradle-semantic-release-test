d4a6401d3909bf81826aafe32badbed87d2290ce
alexsedova <als@praqma.net>
2016-11-22 12:59:17 +0100

    feat: Add new func

--- src/main/java/de/gliderpilot/semanticrelease/test/Test.java
+++ src/main/java/de/gliderpilot/semanticrelease/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
         return hello("World!");
     }
 
-    public static String helloAll() { return hello("all!"); }
+    public static String helloAll() { return hello("everyone!"); }
 
     public static String hello(String who) {
         return "Hello " + who;
